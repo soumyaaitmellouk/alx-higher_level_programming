801300198928dbdebc1cfd3c772358f00ec9af7a
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-11-30 11:18:07 -0800

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argNum = len(sys.argv) - 1
+    if argNum == 0:
+        print("0 arguments.")
+    elif argNum == 1:
+        print("1 argument.")
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{} arguments.".format(argNum))
+        for r in range(1, argNum + 1):
+            print("{}: {}".format(r, sys.argv[r]))
