64d69afcd4a944cff75b5460723c00da39c747ad
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-11-30 09:09:58 -0800

    task0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def add(a, b):
-    somme = a + b
-    print("{} + {} = {}".format(a, b, somme))
+from  add_0 import add
+a = 1
+b = 2
+print("{} + {} = {}".format(a, b, add(a, b)))
