332b6893110e6f63ce2eb0d89c93b910bd58c594
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-11-30 11:27:26 -0800

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     if argNum == 0:
         print("0 arguments.")
     elif argNum == 1:
-        print("1 argument.")
+        print("1 argument:")
         print("1: {}".format(sys.argv[1]))
     else:
-        print("{} arguments.".format(argNum))
+        print("{} arguments:".format(argNum))
         for r in range(argNum):
             print("{}: {}".format(r + 1, sys.argv[r + 1]))
