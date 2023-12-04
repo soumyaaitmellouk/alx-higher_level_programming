60f3e65059189ad689f2a37f92832ceebed3a1e0
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-12-04 10:16:22 -0800

    task5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,5 +5,5 @@ def no_c(my_string):
         if my_string[i] == 'c' or my_string[i] == 'C':
             continue
         else:
-            temp= temp + my_string[i]
+            temp = temp + my_string[i]
     return temp
