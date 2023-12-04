bb006c30aad6a6ca006b9154517ed16bee0eacd1
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-12-04 11:08:08 -0800

    task6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix == []:
-        print("")
-    else:
         for j in range(len(matrix)):
             for i in range(len(matrix[j])):
                 if i < len(matrix[j]) - 1:
