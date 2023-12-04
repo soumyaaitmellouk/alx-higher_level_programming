72094697a589dbe7c60c2efeac1cfb53e696afe3
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-12-04 11:18:13 -0800

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     temp = ()
-    first = tuple_a + (0. 0)
-    seconde = tuple_b + (0. 0)
-    temp = first[0] + seconde[0]. first[1] + seconde[1]
+    first = tuple_a + (0, 0)
+    seconde = tuple_b + (0, 0)
+    temp = first[0] + seconde[0], first[1] + seconde[1]
     return temp
