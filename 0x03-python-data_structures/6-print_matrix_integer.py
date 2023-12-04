20d814814b2ea444e86a63453beae71a4b0b2dea
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-12-04 11:02:06 -0800

    task6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,4 +6,5 @@ def print_matrix_integer(matrix=[[]]):
         for j in range(len(matrix)):
             for i in range(len(matrix[j])):
                 print("{}".format(matrix[j][i]), end=" ")
+          
             print()
