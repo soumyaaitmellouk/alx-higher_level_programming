e1694b2404c001f5dfd5a1f538153dfba3c3f892
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-12-03 16:05:37 -0800

    task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    temp = my_list.copy()
+    if idx < 0 or idx > len(my_list) - 1:
+        retun my_list.copy()
+    else:
+        temp[idx] = element
+        return temp
