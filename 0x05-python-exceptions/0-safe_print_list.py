32d09caf74e7d583039496ba1f96cacfb591b9cb
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-12-19 11:25:33 -0800

    task0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print("{}".format(my_list[i]), end="")
             j = j + 1
-    except:
+    except IndexError:
         pass
     print(" ")
     return j
