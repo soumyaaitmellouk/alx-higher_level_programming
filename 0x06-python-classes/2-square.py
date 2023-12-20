157b1b699fea4ac26c7c0e2b0938fe1dcbb644f6
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-12-20 10:43:03 -0800

    task2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,6 +5,12 @@
 class Square:
     """class define a square"""
     def __init__(self, size=0):
+        """
+         Instantiate square  with a specified size
+
+         Args:
+             size (int): Size of the instance.
+        """
         if (type(size) == int):
             if (size < 0):
                 raise ValueError("size must be >= 0")
