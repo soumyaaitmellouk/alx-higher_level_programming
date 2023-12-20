c5b75ec57517c6cb9a233f43159f8dbe872de444
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2023-12-20 11:41:28 -0800

    task4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""a class Square that defines a square"""
+
+
+class Square:
+    """class define a square"""
+
+    def __init__(self, size=0):
+        """
+         Instantiate square  with a specified size
+
+         Args:
+             size (int): Size of the instance.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """to retrieve the size
+        return: the size
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """
+         to set value
+        :param value: the size
+        """
+        if (type(value) == int):
+            if (value < 0):
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """
+         Public instance method that returns the current square area
+        :return:
+            the area of square
+        """
+        return (self.__size ** 2)
