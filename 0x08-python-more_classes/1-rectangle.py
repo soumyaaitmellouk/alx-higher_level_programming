cf4f4e5b54f594d8a8aa4db7e2f09f2d3100e85e
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2024-01-05 08:44:51 -0800

    task1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Class Define a rectangle"""
+
+
+class Rectangle:
+    """ class define a rectagle"""
+
+    def __init__(self, width=0, height=0):
+        """
+        initalize the attributes
+        :param width: the width of rectangle
+        :param height: the height of rectangle
+        """
+        self.width = width
+        self.height = height
+    @property
+    def width(self):
+        """
+        to retrieve the width
+        :return: the width
+        """
+        return self.__width
+    @width.setter
+    def width(self, value):
+        """
+                to set value of width
+                :param value: the width
+        """
+        if (type(value) != int):
+            raise TypeError("width must be an integer")
+        else:
+            if (value < 0):
+                raise ValueError ("width must be >= 0")
+        self.__width = value
+    @property
+    def height(self):
+        """
+        set the height of rectangle
+        :return: the height
+        """
+        return self.__height
+    @height.setter
+    def height(self, value):
+        """
+                 to set value of height
+                :param value: the height
+        """
+        if (type(value) != int):
+            raise TypeError("height must be an integer")
+        else:
+            if (value < 0):
+                raise ValueError ("height must be >= 0")
+        self.__height = value
