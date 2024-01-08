d3494adae0ad6b3dcdd17bd73508d0496193b4ca
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2024-01-08 15:45:00 -0800

    task6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+class to some area calcul
+"""
+
+
+class BaseGeometry:
+    """
+    Base Geometry class
+    """
+
+    def area(self):
+        """not implemanted"""
+        raise Exception("area() is not implemented")
