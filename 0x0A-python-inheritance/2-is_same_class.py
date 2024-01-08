2989df1819aec568d3bbdb1b1356e4827349b9e2
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2024-01-08 14:31:23 -0800

    task2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+function to return a type
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    function to return a type
+    :param obj: parameter 1
+    :param a_class: parameter 2
+    :return: True or False
+    """
+    return isinstance(obj, a_class)
