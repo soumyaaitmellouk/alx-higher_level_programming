1b6e9760faed063806f045cce45677ae04a64d08
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2024-01-09 14:10:04 -0800

    tasks

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-"""Defines a file-appending function."""
+"""Defines a JSON file-writing function."""
+import json
 
 
-def append_write(filename="", text=""):
-    """Appends a string to the end of a UTF8 text file.
-
-    Args:
-        filename (str): The name of the file to append to.
-        text (str): The string to append to the file.
-    Returns:
-        The number of characters appended.
-    """
-    with open(filename, "a", encoding="utf-8") as f:
-        return f.write(text)
+def save_to_json_file(my_obj, filename):
+    """Write an object to a text file using JSON representation."""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
