9d7673ea6a114fb0654d0a917d99b1f2bf0a9949
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2024-01-11 12:21:35 -0800

    task1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+class Base:
+    """class base"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        function init
+        :param id: parameter
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
