14cc098af6068863593aaee51627a82183509922
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2024-03-21 05:19:08 -0700

    tasks

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                               passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    curs = database.cursor()
+    matchy = sys.argv[4]
+    curs.execute("SELECT * FROM states WHERE name LIKE %s", (matchy, ))
+    rows = curs.fetchall()
+    for i in rows:
+        print(i)
+    curs.close()
+    database.close()
