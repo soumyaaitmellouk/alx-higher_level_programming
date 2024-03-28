c81e8ad7083556794baad5bd7d02506fa0e55f94
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2024-03-28 16:22:52 -0700

    task0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,2 +1,13 @@
-2
-2
+#!/bin/bash
+
+# Check if URL argument is provided
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+url=$1
+response=$(curl -s -o /tmp/response.txt -w "%{size_download}" "$url")
+
+echo "Size of the response body: $response bytes"
+rm -f /tmp/response.txt
