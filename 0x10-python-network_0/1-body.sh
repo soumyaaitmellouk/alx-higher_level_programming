00ffbc4db834724372d51bff05a52498659c1ad6
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2024-03-29 16:00:38 -0700

    task1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
-
-# Check if URL argument is provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-url=$1
-
-# Send a GET request to the URL and display the body of the response if status code is 200
-response=$(curl -s -w "%{http_code}" -o /tmp/response.txt "$url")
-status_code=$(tail -n1 /tmp/response.txt)
-
-if [ $status_code -eq 200 ]; then
-    cat /tmp/response.txt
-fi
-
-# Clean up the temporary file
-rm -f /tmp/response.txt
+# sends a request to that URL, and displays the size of the body of the response
+curl -sX GET $1 -L
