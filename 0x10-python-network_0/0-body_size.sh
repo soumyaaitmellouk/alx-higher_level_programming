b837c046d351fe7003701b5a43a10a258a017602
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2024-03-29 15:45:39 -0700

    task0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-
 # Check if URL argument is provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-url=$1
-
-# Send a request to the URL and display the size of the response body in bytes
-curl -sI "$url" | grep -i 'Content-Length' | awk '{print $2}'
+curl -sI ALLOW $1 -L | grep "ALLOW" | cut -d " " -f2-
