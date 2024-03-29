dd9136ce4788cb0b45eb75c8a5558c397f3c6e11
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2024-03-28 17:12:17 -0700

    task1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        print(response.headers.get("X-Request-Id"))
