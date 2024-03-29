28bde2f8c02e39ba49365c6319f0ada5a6935e6e
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2024-03-29 16:34:45 -0700

    task3 4

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # sends a POST request to the passed URL, and displays the body of the response
-curl -sX POST $1 -d "email=soumyaaitmellouk222@gmail.com
-&subject=I will always be here for PLD" -L
+curl -sX POST $1 -d "email=test@gmail.com&subject=I will always be here for PLD" -L
