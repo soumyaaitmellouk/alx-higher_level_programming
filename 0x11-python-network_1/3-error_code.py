dc601ac2820ed098a95f2943ea615cd99639be21
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2024-03-29 17:01:24 -0700

    task

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""
+script that takes in a URL, sends a request to the URL and displays the
+body of the response (decoded in utf-8).
+You have to manage urllib.error.HTTPError exceptions and
+print: Error code: followed by the HTTP status code.
+"""
 import urllib.request
 import urllib.error
 import sys
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
