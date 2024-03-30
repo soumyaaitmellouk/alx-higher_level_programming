36032525026df6b2c7838dc04e8a76abe470d835
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2024-03-30 16:24:21 -0700

    tasks

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-Write a Python script that takes in a letter and sends a POST request 
+Write a Python script that takes in a letter and sends a POST request
 to http://0.0.0.0:5000/search_user with the letter as a parameter.
 
 The letter must be sent in the variable q
 If no argument is given, set q=""
-If the response body is properly JSON formatted and not empty, 
+If the response body is properly JSON formatted and not empty,
 display the id and name like this: [<id>] <name>
 Otherwise:
 Display Not a valid JSON if the JSON is invalid
